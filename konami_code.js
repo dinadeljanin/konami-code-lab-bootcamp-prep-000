c805c702f91c00e568bc9f2ccfd569c5e00edbcf
D D <ddeljanin5@icloud.com>
2020-03-09 22:53:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,19 +15,16 @@ const codes = [
 function init() {
   // has to match the length of codes to alert
   let index = 0;
-  
+
   document.body.addEventListener("keydown", (event) => {
     console.log(event.key);
     if (event.key === codes[index]) {
-      index++;
-      if (index === codes.length) {
-        alert("What do you think you're doing!? STOP FOOLIN' AROUND KID!");
-        index = 0;
-      }
-    } else {
+      ++index;
+      console.log(index);
+
+    } if (index === codes.length-1) {
+      alert("What do you think you're doing!? STOP FOOLIN' AROUND KID!");
       index = 0;
     }
   });
 }
-
-init();
